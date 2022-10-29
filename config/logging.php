24030fa7e7a744c11ba2b2794783cc0bceeb7972
oscar <oscar.sanchez@novacloud.mx>
2022-10-29 12:05:47 -0500

    fi x

--- config/logging.php
+++ config/logging.php
@@ -72,7 +72,7 @@ return [
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
             'username' => 'Laravel Log',
             'emoji' => ':boom:',
-            'level' => 'error',
+            'level' => 'critical',
         ],
 
         'papertrail' => [
